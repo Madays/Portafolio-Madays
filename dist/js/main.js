6c2061a723c4e275767cbcd3184b66acb5b5a98a
Madays <maday.choque@gmail.com>
2017-05-01 16:47:04 -0500

    tst about hero

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,18 @@
 var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
+$(document).ready(init);
+function init()
+{
+    moveLogo();
+    
+}
+function moveLogo(){
+    $('#layer6').addClass('img-start');
+    $('#layer6').css('transition','.8s');
+    $('#layer6').addClass('img-end');
+}
+function aleatorio(min,max){
+    var resultado;
+    resultado = Math.floor(Math.random()*(max-min))+min;
+    return resultado;
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,22 @@ var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
 $(document).ready(init);
 function init()
-{
-    $(document).click(moveLogo);
+{    
+    //setInterval(moveLogo,2000);
+    
 }
-function moveLogo(){
-    alert('df');
-}
+/*function moveLogo(){
+    $('li#liLayer6').html('<img src="https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png" id="layer6">');
+    setInterval(function(){
+        //$('img#layer6').removeAttr("style");
+        $('img#layer6').css({
+        'width':'40px',                      'transform':'translate(0,110px)',        'transition':'.8s',
+        'width':'30px',
+        'transform':'translate(0,90px)'
+    });
+    },1000);    
+};*/
+
 function aleatorio(min,max){
     var resultado;
     resultado = Math.floor(Math.random()*(max-min))+min;
